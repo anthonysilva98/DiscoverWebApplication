a8863a39408cb250077ed03f7c89da1cfb1f647a
anthonysilva98 <anthonyssilva98@gmail.com>
2023-10-26 17:53:30 -0700

    Browser Tab init

---
+++ app/(tabs)/Browser.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Browser = () => {
+  return (
+    <View>
+      <Text>Browser</Text>
+    </View>
+  );
+};
+
+export default Browser;
+
+const styles = StyleSheet.create({});
