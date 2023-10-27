51e2c75bedb37276cb9ac9ce41314598e2f465d8
anthonysilva98 <anthonyssilva98@gmail.com>
2023-10-26 20:50:37 -0700

    Screen Displaying wallet w/out functionality

--- app/(tabs)/Wallet.tsx
+++ app/(tabs)/Wallet.tsx
@@ -1,11 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Touchable,
+  ScrollView,
+} from "react-native";
 import React from "react";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
+import WalletButton from "../../components/Wallet/WalletButton";
+import FiatDisplay from "../../components/Wallet/FiatDisplay";
 const Wallet = () => {
   return (
-    <View>
-      <Text>Wallet</Text>
-    </View>
+    <SafeAreaView style={tw`bg-background`}>
+      <View style={tw`justify-between items-center flex-row bg-gray-800 p-2`}>
+        <Image
+          source={require("../../assets/images/WalletAssets/slider.png")}
+        />
+        <Image
+          source={require("../../assets/images/WalletAssets/qrscanner.png")}
+        />
+      </View>
+      <View style={tw`pt-20 items-center`}>
+        <View style={tw`flex-row justify-center`}>
+          <Text style={tw`text-white font-semibold pr-5`}>TOTAL BALANCE</Text>
+          <Image
+            style={tw`size-10`}
+            source={require("../../assets/images/WalletAssets/eye.png")}
+          />
+        </View>
+        <Text style={tw`text-white pt-5`}>$ 44,346.95</Text>
+      </View>
+      <View style={tw`justify-center items-center flex-row p-10`}>
+        <View style={tw`px-3`}>
+          <View
+            style={tw`bg-blue-500 h-20 w-20 rounded-full justify-center items-center`}
+          >
+            <WalletButton />
+          </View>
+        </View>
+        <View style={tw`px-3`}>
+          <View
+            style={tw`bg-blue-500 h-20 w-20 rounded-full justify-center items-center`}
+          >
+            <WalletButton />
+          </View>
+        </View>
+        <View style={tw`px-3`}>
+          <View
+            style={tw`bg-blue-500 h-20 w-20 rounded-full justify-center items-center`}
+          >
+            <WalletButton />
+          </View>
+        </View>
+      </View>
+
+      <ScrollView style={tw`bg-gray-800 h-200 mt-10 rounded-t-md`}>
+        <FiatDisplay />
+        <FiatDisplay />
+        <FiatDisplay />
+        <FiatDisplay />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
